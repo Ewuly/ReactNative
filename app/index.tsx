26f1a011f6c3a065f92603f96b4f744ebdf593ca
Ewan <ewan.hamon@gmail.com>
2024-08-09 16:31:37 +0200

    feat: FlatList

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,32 @@
 import { Text, View } from "react-native";
-import { Image, StyleSheet, Platform, ScrollView } from 'react-native';
+import { Image, StyleSheet, Platform, FlatList, ScrollView } from 'react-native';
 import React, { useState } from "react";
 
 
 export default function Index() {
   const [people, setPeople] = useState([
-    { name: 'ewan1', key: '1'},
-    { name: 'ewan2', key: '2'},
-    { name: 'ewan3', key: '3'},
-    { name: 'ewan4', key: '4'},
-    { name: 'ewan5', key: '5'},
-    { name: 'ewan6', key: '6'},
-    { name: 'ewan7', key: '7'},
-    { name: 'ewan8', key: '8'},
+    { name: 'ewan1', id: '1'},
+    { name: 'ewan2', id: '2'},
+    { name: 'ewan3', id: '3'},
+    { name: 'ewan4', id: '4'},
+    { name: 'ewan5', id: '5'},
+    { name: 'ewan6', id: '6'},
+    { name: 'ewan7', id: '7'},
+    { name: 'ewan8', id: '8'},
   ])
 
 
   return (
     <View style={styles.container}>
-      
-      <ScrollView>
-        { people.map((item) => (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          ))}
-      </ScrollView>
+
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
 
     </View>
   );
@@ -43,6 +44,7 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: 'pink',
     fontSize: 24,
+    marginHorizontal:10,
   },
 
 })
