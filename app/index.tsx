e062d2394aca23bc025e5b82419735cf3feedd2a
Ewan <ewan.hamon@gmail.com>
2024-08-12 14:42:26 +0200

    feat: alert if small word and keyboard dismiss

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from "react-native";
 import Header from "./components/header";
 import TodoItem from "./components/todoItem";
 import AddTodo from "./components/addTodo";
@@ -7,9 +7,9 @@ import AddTodo from "./components/addTodo";
 
 export default function Index() {
   const [todos, setTodos] = useState([
-    {text: 'buy coffee', key: '1'},
-    {text: 'buy tea', key: '2'},
-    {text: 'buy biscuit', key: '3'}
+    { text: 'buy coffee', key: '1' },
+    { text: 'buy tea', key: '2' },
+    { text: 'buy biscuit', key: '3' }
   ])
 
   const pressHandler = (key: string) => {
@@ -19,29 +19,41 @@ export default function Index() {
   }
 
   const submitHandler = (text: string) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ];
-    })
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      })
+    }
+    else {
+      Alert.alert('OUPS', 'Todos must be over 3 characters long', [
+        { text: 'Understood', onPress: () => console.log('alert closed') }
+      ])
+    }
   }
 
   return (
-    <View style={styles.container}>
-      <Header/>
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler}/>
-        <View style={styles.list}>
-          <FlatList
-          data={todos}
-          renderItem={({ item }) => (
-            <TodoItem item={item} pressHandler={pressHandler} />
-          )}
-          />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismiss keyboard');
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
